f3846264e357f6c5dd73de2e84471e62fe235500
Pierre Ricadat <ghostdogpr@gmail.com>
2022-07-31 10:00:08 +0900

    Update to the latest 1.x

--- build.sbt
+++ build.sbt
@@ -5,10 +5,11 @@ version := "0.1"
 scalaVersion := "2.13.3"
 
 libraryDependencies ++= Seq(
-  "com.github.ghostdogpr"         %% "caliban"                       % "1.2.1",
-  "com.github.ghostdogpr"         %% "caliban-http4s"                % "1.2.1",
-  "com.github.ghostdogpr"         %% "caliban-client"                % "1.2.1",
-  "com.softwaremill.sttp.client3" %% "async-http-client-backend-zio" % "3.3.15"
+  "com.github.ghostdogpr"         %% "caliban"                       % "1.4.3",
+  "com.github.ghostdogpr"         %% "caliban-http4s"                % "1.4.3",
+  "com.github.ghostdogpr"         %% "caliban-client"                % "1.4.3",
+  "org.http4s"                    %% "http4s-blaze-server"           % "0.23.12",
+  "com.softwaremill.sttp.client3" %% "async-http-client-backend-zio" % "3.3.18"
 )
 
 enablePlugins(CalibanPlugin)
--- src/main/scala/part1/HttpApp.scala
+++ src/main/scala/part1/HttpApp.scala
@@ -6,18 +6,22 @@ import org.http4s.implicits._
 import org.http4s.server.Router
 import org.http4s.server.middleware.CORS
 import zio.interop.catz._
-import zio.{ ExitCode, Task, ZIO }
+import zio._
+import zio.blocking.Blocking
+import zio.clock.Clock
 
 object HttpApp extends CatsApp {
+
+  type MyTask[A] = RIO[Clock with Blocking, A]
 
   override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, ExitCode] =
     MyApi.interpreter
       .flatMap(
         interpreter =>
-          BlazeServerBuilder[Task]
+          BlazeServerBuilder[MyTask]
             .bindHttp(8088, "localhost")
             .withHttpApp(
-              Router[Task]("/api/graphql" -> CORS.policy(Http4sAdapter.makeHttpService(interpreter))).orNotFound
+              Router[MyTask]("/api/graphql" -> CORS.policy(Http4sAdapter.makeHttpService(interpreter))).orNotFound
             )
             .resource
             .toManagedZIO
